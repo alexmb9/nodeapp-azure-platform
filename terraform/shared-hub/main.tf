aeecf380c0f29722f4134772133d124543b29b8d
alexmb9 <alex.baxter99@outlook.com>
2025-12-13 15:52:50 +0000

    Added Hub VNet and firewall subnet, variables for Hub Vnet, firewall subnet, firewall, public IP

--- terraform/shared-hub/main.tf
+++ terraform/shared-hub/main.tf
@@ -3,3 +3,20 @@ resource "azurerm_resource_group" "shared" {
   location = var.location
   tags     = var.tags
 }
+
+#vhub net
+resource "azurerm_virtual_network" "hub" {
+  name                = var.hub_vnet_name
+  location            = azurerm_resource_group.shared.location
+  resource_group_name = azurerm_resource_group.shared.name
+  address_space       = var.hub_vnet_cidr
+  tags                = var.tags
+}
+
+#firewall subnet
+resource "azurerm_subnet" "azure_firewall_subnet" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.shared.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = [var.fw_subnet_cidr]
+}
--- terraform/shared-hub/variables.tf
+++ terraform/shared-hub/variables.tf
@@ -1,11 +1,11 @@
-variable "location" {
-  type = string
-}
+#All resource vars
+variable "location" { type = string }
+variable "environment" { type = string }
+variable "tags" { type = map(string) }
 
-variable "environment" {
-  type = string
-}
-
-variable "tags" {
-  type = map(string)
-}
+#Hub Vnet and Azure Firewall vars
+variable "hub_vnet_name" { type = string }
+variable "hub_vnet_cidr" { type = list(string) }     # e.g. ["10.0.0.0/16"]
+variable "fw_subnet_cidr" { type = string }          # e.g. "10.0.1.0/26"
+variable "fw_name" { type = string }
+variable "fw_public_ip_name" { type = string }
